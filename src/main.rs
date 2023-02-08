0b8182f795dd459eae7f59155ecbce7a2db1e8d6
Adam Tokarsky <36281850+adtok@users.noreply.github.com>
2023-02-08 02:49:52 +0000

    entry control flow

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use std::env;
+
+fn run_file(file: &str) {
+    println!("Running file {}...", file);
+}
+
+fn run_prompt() {
+    println!("Running repl");
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() > 2 {
+        println!("Usage: lox [script]");
+    } else if args.len() == 2 {
+        run_file(&args[1]);
+    } else {
+        run_prompt();
+    }
 }
