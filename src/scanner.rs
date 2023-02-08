02e78b97bbf482483105e6b052ccd5fe2f2158cb
Adam Tokarsky <36281850+adtok@users.noreply.github.com>
2023-02-08 04:24:12 +0000

    Lots of scanning

--- src/scanner.rs
+++ src/scanner.rs
@@ -1,16 +1,145 @@
-pub struct Scanner;
+pub struct Scanner {
+    source: String,
+    tokens: Vec<Token>,
+    start: usize,
+    current: usize,
+    line: usize,
+}
 
 impl Scanner {
-    pub fn new(_source: &str) -> Self {
-        Self {}
+    pub fn new(source: &str) -> Self {
+        Self {
+            source: source.to_string(),
+            tokens: vec![],
+            start: 0,
+            current: 0,
+            line: 1,
+        }
     }
 
-    pub fn scan_tokens(&self) -> Result<Vec<Token>, String> {
-        todo!()
+    pub fn scan_tokens(&mut self) -> Result<Vec<Token>, String> {
+        let mut errors = vec![];
+        while !self.is_at_end() {
+            self.start = self.current;
+            match self.scan_token() {
+                Ok(_) => (),
+                Err(msg) => errors.push(msg),
+            }
+        }
+
+        self.tokens
+            .push(Token::new(TokenType::Eof, "".to_string(), None, self.line));
+
+        Ok(self.tokens.clone())
+    }
+
+    fn scan_token(&mut self) -> Result<(), String> {
+        let c = self.advance();
+
+        match c {
+            '(' => self.add_token(TokenType::LeftParen),
+            ')' => self.add_token(TokenType::RightParen),
+            '{' => self.add_token(TokenType::LeftBrace),
+            '}' => self.add_token(TokenType::RightBrace),
+            ',' => self.add_token(TokenType::Comma),
+            '.' => self.add_token(TokenType::Dot),
+            '-' => self.add_token(TokenType::Minus),
+            '+' => self.add_token(TokenType::Plus),
+            ';' => self.add_token(TokenType::Semicolon),
+            '*' => self.add_token(TokenType::Star),
+            '!' => {
+                if self.char_match('=') {
+                    self.add_token(TokenType::BangEqual)
+                } else {
+                    self.add_token(TokenType::Bang)
+                }
+            }
+            '=' => {
+                if self.char_match('=') {
+                    self.add_token(TokenType::EqualEqual)
+                } else {
+                    self.add_token(TokenType::Equal)
+                }
+            }
+            '<' => {
+                if self.char_match('=') {
+                    self.add_token(TokenType::LessEqual)
+                } else {
+                    self.add_token(TokenType::Less)
+                }
+            }
+            '>' => {
+                if self.char_match('=') {
+                    self.add_token(TokenType::GreaterEqual)
+                } else {
+                    self.add_token(TokenType::Greater)
+                }
+            }
+            '/' => {
+                if self.char_match('/') {
+                    while self.peek() != '\n' && !self.is_at_end() {
+                        self.advance();
+                    }
+                } else {
+                    self.add_token(TokenType::Slash)
+                }
+            }
+            ' ' | '\r' | '\t' => {}
+            '\n' => self.line += 1,
+            _ => todo!(),
+        }
+
+        Ok(())
+    }
+
+    fn is_at_end(&self) -> bool {
+        self.current >= self.source.len()
+    }
+
+    fn advance(&mut self) -> char {
+        let c = self.source.chars().nth(self.current).unwrap();
+        self.current += 1;
+
+        c
+    }
+
+    fn add_token(&mut self, token_type: TokenType) {
+        self.add_token_lit(token_type, None);
+    }
+
+    fn add_token_lit(&mut self, token_type: TokenType, literal: Option<LiteralValue>) {
+        let text = self.source[self.start..self.current].to_string();
+
+        self.tokens.push(Token {
+            token_type: token_type,
+            lexeme: text,
+            literal: literal,
+            line: self.line,
+        })
+    }
+
+    fn char_match(&mut self, expected: char) -> bool {
+        if self.is_at_end() {
+            return false;
+        }
+        if self.source.chars().nth(self.current).unwrap() != expected {
+            return false;
+        }
+
+        self.current += 1;
+        true
+    }
+
+    fn peek(&self) -> char {
+        if self.is_at_end() {
+            '\0'
+        } else {
+            self.source.chars().nth(self.current).unwrap()
+        }
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub enum LiteralValue {
     IntValue(i64),
     FValue(f64),
@@ -18,12 +147,12 @@ pub enum LiteralValue {
     IdentiferValue(String),
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct Token {
     token_type: TokenType,
     lexeme: String,
     literal: Option<LiteralValue>,
-    line: u64,
+    line: usize,
 }
 
 impl Token {
@@ -31,7 +160,7 @@ impl Token {
         token_type: TokenType,
         lexeme: String,
         literal: Option<LiteralValue>,
-        line: u64,
+        line: usize,
     ) -> Self {
         Self {
             token_type,
@@ -40,9 +169,13 @@ impl Token {
             line,
         }
     }
+
+    pub fn to_string(&self) -> String {
+        format!("{} {} {:?}", self.token_type, self.lexeme, self.literal)
+    }
 }
 
-#[derive(Debug)]
+#[derive(Debug, Copy, Clone, PartialEq)]
 pub enum TokenType {
     // Single-character tokens.
     LeftParen,
@@ -90,4 +223,10 @@ pub enum TokenType {
     While,
 
     Eof,
+}
+
+impl std::fmt::Display for TokenType {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(f, "{:?}", self)
+    }
 }
