0167f923e9b966eb014a1f2967049bc74bd67232
Adam Tokarsky <36281850+adtok@users.noreply.github.com>
2023-02-10 09:50:28 -0500

    impl to_string for Stmt

--- src/statement.rs
+++ src/statement.rs
@@ -6,3 +6,16 @@ pub enum Stmt {
     Print { expression: Expr },
     Var { name: Token, initializer: Expr },
 }
+
+impl ToString for Stmt {
+    fn to_string(&self) -> String {
+        match self {
+            Stmt::Expression { expression } => expression.to_string(),
+            Stmt::Print { expression } => format!("(print {})", expression.to_string()),
+            Stmt::Var {
+                name,
+                initializer: _,
+            } => format!("(var {})", name.to_string()),
+        }
+    }
+}
