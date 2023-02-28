ddc53058c6cec739b8a9245ac724934928cd438c
Adam Tokarsky <36281850+adtok@users.noreply.github.com>
2023-02-28 11:44:30 -0500

    refactor interpreter

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -198,7 +198,7 @@ impl Interpreter {
 
                     for stmt in body.iter() {
                         lambda_int
-                            .interpret(vec![stmt])
+                            .execute(stmt)
                             .unwrap_or_else(|_| panic!("Evaluating field failed"));
                         if let Some(value) = lambda_int.specials.get("return") {
                             return value.clone();
@@ -254,100 +254,104 @@ impl Interpreter {
 
     pub fn interpret(&mut self, stmts: Vec<&Stmt>) -> Result<(), String> {
         for stmt in stmts {
-            match stmt {
-                Stmt::Block { statements } => {
-                    let mut new_environment = Environment::new();
-                    new_environment.enclosing = Some(self.environment.clone());
-                    let old_environment = self.environment.clone();
-                    self.environment = Rc::new(RefCell::new(new_environment));
-                    let block_result = self.interpret(statements.iter().collect());
-                    self.environment = old_environment;
-                    block_result?
-                }
-                Stmt::Expression { expression } => {
-                    self.evaluate(expression)?;
-                }
-                Stmt::Function { name, params, body } => {
-                    let arity = params.len();
-
-                    let params: Vec<Token> = params.iter().map(|t| (*t).clone()).collect();
-                    let body: Vec<Stmt> = body.iter().map(|b| (*b).clone()).collect();
-                    let name_clone = name.lexeme.clone();
-
-                    let parent_env = self.environment.clone();
-                    let fun_impl = move |args: &[LiteralValue]| {
-                        let mut closure_int = Interpreter::for_closure(parent_env.clone());
-                        for (i, arg) in args.iter().enumerate() {
-                            closure_int
-                                .environment
-                                .borrow_mut()
-                                .define(params[i].lexeme.clone(), (*arg).clone());
-                        }
-
-                        for item in &body {
-                            closure_int.interpret(vec![item]).unwrap_or_else(|_| {
-                                panic!("Evaluating failed inside {name_clone}.")
-                            });
-
-                            if let Some(value) = closure_int.specials.get("return") {
-                                return value.clone();
-                            }
-                        }
-
-                        LiteralValue::Nil
-                    };
-
-                    let callable = LiteralValue::Callable {
-                        name: name.lexeme.clone(),
-                        arity,
-                        fun: Rc::new(fun_impl),
-                    };
-
-                    self.environment
-                        .borrow_mut()
-                        .define(name.lexeme.clone(), callable);
-                }
-                Stmt::If {
-                    condition,
-                    then_stmt,
-                    else_stmt,
-                } => {
-                    let truth_value = self.evaluate(condition)?;
-                    if truth_value.is_truthy() {
-                        self.interpret(vec![then_stmt])?
-                    } else if let Some(els) = else_stmt {
-                        self.interpret(vec![els])?
-                    };
-                }
-                Stmt::Print { expression } => {
-                    let result = self.evaluate(expression)?;
-                    println!("{result}");
-                }
-                Stmt::Return { keyword: _, value } => {
-                    let value = if let Some(expr) = value {
-                        self.evaluate(expr)?
-                    } else {
-                        LiteralValue::Nil
-                    };
-                    self.specials.insert(String::from("return"), value);
-                }
-                Stmt::Var { name, initializer } => {
-                    let value = self.evaluate(initializer)?;
-                    self.environment
-                        .borrow_mut()
-                        .define(name.lexeme.clone(), value);
-                }
-                Stmt::While { condition, body } => {
-                    let mut flag = self.evaluate(condition)?;
-                    while flag.is_truthy() {
-                        let statements: Vec<&Stmt> = vec![body.as_ref()];
-                        self.interpret(statements)?;
-                        flag = self.evaluate(condition)?;
-                    }
-                }
-            };
+            self.execute(stmt)?
         }
+        Ok(())
+    }
 
+    fn execute(&mut self, stmt: &Stmt) -> Result<(), String> {
+        match stmt {
+            Stmt::Block { statements } => {
+                let mut new_environment = Environment::new();
+                new_environment.enclosing = Some(self.environment.clone());
+                let old_environment = self.environment.clone();
+                self.environment = Rc::new(RefCell::new(new_environment));
+                let block_result = self.interpret(statements.iter().collect());
+                self.environment = old_environment;
+                block_result?
+            }
+            Stmt::Expression { expression } => {
+                self.evaluate(expression)?;
+            }
+            Stmt::Function { name, params, body } => {
+                let arity = params.len();
+
+                let params: Vec<Token> = params.iter().map(|t| (*t).clone()).collect();
+                let body: Vec<Stmt> = body.iter().map(|b| (*b).clone()).collect();
+                let name_clone = name.lexeme.clone();
+
+                let parent_env = self.environment.clone();
+                let fun_impl = move |args: &[LiteralValue]| {
+                    let mut closure_int = Interpreter::for_closure(parent_env.clone());
+                    for (i, arg) in args.iter().enumerate() {
+                        closure_int
+                            .environment
+                            .borrow_mut()
+                            .define(params[i].lexeme.clone(), (*arg).clone());
+                    }
+
+                    for item in &body {
+                        closure_int
+                            .execute(item)
+                            .unwrap_or_else(|_| panic!("Evaluating failed inside {name_clone}."));
+
+                        if let Some(value) = closure_int.specials.get("return") {
+                            return value.clone();
+                        }
+                    }
+
+                    LiteralValue::Nil
+                };
+
+                let callable = LiteralValue::Callable {
+                    name: name.lexeme.clone(),
+                    arity,
+                    fun: Rc::new(fun_impl),
+                };
+
+                self.environment
+                    .borrow_mut()
+                    .define(name.lexeme.clone(), callable);
+            }
+            Stmt::If {
+                condition,
+                then_stmt,
+                else_stmt,
+            } => {
+                let truth_value = self.evaluate(condition)?;
+                if truth_value.is_truthy() {
+                    self.execute(then_stmt)?
+                } else if let Some(els) = else_stmt {
+                    self.execute(els)?
+                };
+            }
+            Stmt::Print { expression } => {
+                let result = self.evaluate(expression)?;
+                println!("{result}");
+            }
+            Stmt::Return { keyword: _, value } => {
+                let value = if let Some(expr) = value {
+                    self.evaluate(expr)?
+                } else {
+                    LiteralValue::Nil
+                };
+                self.specials.insert(String::from("return"), value);
+            }
+            Stmt::Var { name, initializer } => {
+                let value = self.evaluate(initializer)?;
+                self.environment
+                    .borrow_mut()
+                    .define(name.lexeme.clone(), value);
+            }
+            Stmt::While { condition, body } => {
+                let mut flag = self.evaluate(condition)?;
+                while flag.is_truthy() {
+                    let statements: Vec<&Stmt> = vec![body.as_ref()];
+                    self.interpret(statements)?;
+                    flag = self.evaluate(condition)?;
+                }
+            }
+        };
         Ok(())
     }
 }
